9d416a3505477bf0f0604af61a27648362fef986
KRISHNA <kraggr2909@gmail.com>
2025-02-03 21:03:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,44 @@
 let gameBoard = document.querySelector(".gameBoard");
 let foodX, foodY;
 let snakeX = 5, snakeY = 5;
+let velocityX = 0, velocityY = 0;
 const changeFood = ()=>{
     foodX = Math.floor(Math.random()*30) + 1;
     foodY = Math.floor(Math.random()*30) + 1;
 }
 const startGame = ()=>{
     let foodHTML = `<div class="food" style="grid-area: ${foodX}/${foodY};"></div>`;
+    snakeX += velocityX;
+    snakeY += velocityY;
+    if(snakeX === foodX && snakeY === foodY){
+        changeFood();
+    }
     foodHTML+= `<div class="snake" style="grid-area: ${snakeX}/${snakeY};"></div>`;
     gameBoard.innerHTML = foodHTML;
 }
 
 changeFood();
-startGame();
+setInterval(startGame , 250);
 document.addEventListener("keydown",(e)=>{
-    if(e.key === "ArrowDown"){
-        snakeY += 1;
-    }else if(e.key === "ArrowUp"){
-
-    }else if(e.key === "ArrowLeft"){
-
-    }else if(e.key === "ArrowRight"){
-
+    let moved = false;
+    if(e.key === "ArrowDown" && velocityX != -1){
+        velocityX = 1;
+        velocityY = 0;
+        moved = true;
+    }else if(e.key === "ArrowUp" && velocityX !=1){
+        velocityX = -1;
+        velocityY = 0;
+        moved = true;
+    }else if(e.key === "ArrowLeft" && velocityY !=1){
+        velocityX = 0;
+        velocityY = -1;
+        moved = true;
+    }else if(e.key === "ArrowRight" && velocityY != -1){
+        velocityX = 0;
+        velocityY = 1;
+        moved = true;
+    }
+    if (moved) {
+        startGame();  // Only run if an arrow key was pressed
     }
 })
